2ab3e4cfe3457033636da72e377d63979c1e4b22
maxlazarus <max.prokopenko@gmail.com>
2015-01-19 01:13:28 -0800

    Start of the cover-letter building functions

---
+++ src/Job.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace jobulator {
+	public class Job {
+		private Dictionary <string, Object> categories = new Dictionary <string, Object>();
+
+		public Job (string id) {
+			if(!Check(id))
+				Add ("id", id);
+		}
+
+		public void Add(string a, Object o) {
+			try {
+				categories.Add (a, o);
+			} catch {
+				//Logger.log("Error adding " + a " of type " + o.ToString())
+			}
+		}
+		public string Get(string a) {
+			return categories [a].ToString ();
+		}
+		public bool Check(string a) {
+			return categories.ContainsKey (a);
+		}
+		public void Print() {
+			foreach(KeyValuePair<string, Object> kvp in categories) {
+				Console.WriteLine (kvp.Key + " : " + (string)kvp.Value);
+			}
+		}
+		public void WriteJSON() {
+			var s = "{";
+			var comma = "";
+			foreach(KeyValuePair<string, Object> kvp in categories) {
+				s += comma;
+				comma = ",";
+				s += Environment.NewLine;
+				s += "\t" + '"' + kvp.Key + '"' + " : " + '"' + (string)kvp.Value + '"';
+			}
+			s += Environment.NewLine + "}";
+			FileHandler.Write (s, this.Get("id") + ".json");
+		}
+		public void Fill(string s) {
+			foreach (var line in s.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)) {
+				KeyValuePair<string, string> kvp = Regexer.ExtractKeyValuePair (line);
+				this.Add (kvp.Key, kvp.Value);
+			}
+		}
+		public static Job fromHTML(string name) {
+			string html = FileHandler.OpenAsString (name + @".html");
+			var jText = Regexer.Convert (html);
+			Job j = new Job (name);
+			j.Fill (jText);
+			return j;
+		}
+		public static Job fromJSON(string name) {
+			System.IO.StreamReader f = FileHandler.Open (name + @".json");				
+			if (f != null) {
+				Job j = new Job (name);
+				string line = "";
+
+				while ((line = f.ReadLine ()) != null) {
+					var m = Regex.Matches (line, "\\\"(.*?)\\\"");
+					var e = m.GetEnumerator ();
+
+					try {
+						e.MoveNext ();
+						var category = Regex.Replace(e.Current.ToString (), "\\\"", "");
+						e.MoveNext ();
+						var data = Regex.Replace(e.Current.ToString (), "\\\"", "");
+						j.Add(category, data);
+					} catch {
+						//unreadable line
+					}
+				}
+				f.Close ();
+				return j;
+			}
+			return null;
+		}
+	}
+}
+
---
+++ src/Jobulator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Gtk;
+
+namespace jobulator {
+	public class Jobulator {
+
+		public Jobulator () {}
+
+		public static void Main() {
+
+			/*
+			var l1 = getJobsFrom ("html");
+			foreach (Job j in l1)
+				j.WriteJSON ();
+				*/
+			var jobs = getJobsFrom ("json");
+			var wordFrequency = new Dictionary<string, int> ();
+			var list = new List<KeyValuePair<string, int>> ();
+
+			foreach(Job j in jobs){
+				string[] words = j.Get("job_requirements")
+					.Split(new string[] { Environment.NewLine, " ", ".", ","  }, StringSplitOptions.RemoveEmptyEntries);
+				foreach (string word in words) {
+					var w = Regex.Match(word, @"[A-Za-z0-9\-\']+").Groups[0].Value.ToLower();
+					if (wordFrequency.ContainsKey (w))
+						wordFrequency [w] = (wordFrequency [w]) + 1;
+					else
+						wordFrequency.Add (w, 1);
+				}
+			}
+			foreach (KeyValuePair<string, int> kvp in wordFrequency)
+				list.Add (kvp);
+			list.Sort(delegate(KeyValuePair<string, int> x, KeyValuePair<string, int> y){
+				return y.Value.CompareTo(x.Value);
+			});
+			var output = "";
+			foreach(KeyValuePair<string, int> kvp in list) {
+				output += kvp.Key + " " + kvp.Value + "<br>" + Environment.NewLine;
+			}
+			FileHandler.Write (output, "wordlist.txt");
+			/*
+			foreach (Job j in jobs) {
+				if (j.Get ("job_location").ToLower().Contains ("vancouver")) {
+					string title = j.Get ("job_title");
+					Console.WriteLine (title);				
+				}
+			}
+			*/
+
+			//Console.WriteLine ("# of html files = "l1.Count + ", # of JSON files = " + l2.Count);
+
+			/*
+			CoverLetter cl = new CoverLetter (l2[0]);
+			cl.printDOCX ();
+			*/
+
+			//StartGUI ();
+		}
+		public static List<Job> getJobsFrom(string fileType) {
+			var jobList = new List<Job> ();
+
+			int i = 1;
+			var jobs = FileHandler.getFileNames (fileType);
+			foreach (string s in jobs) {
+				//Console.Clear ();
+				Console.Write ("Converting job " + i++ + " of " + jobs.Count + Environment.NewLine);
+				var name = Path.GetFileName(s);
+				string id = System.Text.RegularExpressions.Regex.Replace (name, "\\..*", "");
+				Job j = new Job ("test");
+				if(fileType.ToLower() == "html")
+					jobList.Add (j = Job.fromHTML(id));
+				if(fileType.ToLower() == "json")
+					jobList.Add (Job.fromJSON(id));
+			}
+			return jobList;
+		}
+		public static void StartGUI (){
+			Application.Init ();
+			Window myWin = new Window ("Jobulator");
+			myWin.Resize (300, 500);
+
+			Button b;
+			var vBox = new VBox ();
+			/*
+			foreach (Job j in jobs){
+				b = new Button ();
+				b.Label = j.Get ("id");
+				vBox.Add (b);
+			}
+			*/
+			myWin.Add (vBox);
+			myWin.ShowAll();
+
+			Application.Run();
+		}
+	}
+}
+
