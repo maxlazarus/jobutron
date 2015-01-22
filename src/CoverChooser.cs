ad807a9175ecdfcaaa64240edeb9bc44c764ff37
Brad MacNeil <brad.macneil5@gmail.com>
2015-01-21 16:16:27 -0800

    Created class cover chooser

---
+++ src/CoverChooser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace jobulator {
+	public class CoverChooser {
+		public CoverChooser () {
+		}
+		public static string Choose( Job j ) {
+			if (
+				j.CategoryContains ("job_title", "software") |
+				j.CategoryContains ("job_title", "developer") |
+				j.CategoryContains ("industry", "software")) { 
+				return "software.cover";
+			} else {
+				return "basic.cover";
+			}
+		}
+	}
+}
+
